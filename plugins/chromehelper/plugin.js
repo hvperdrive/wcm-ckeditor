045c95989bfdd72224c685378d43467c311f1991
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-04-24 13:02:37 +0200

    feat(plugins): add chromehelper

---
+++ plugins/chromehelper/plugin.js
@@ -0,0 +1,54 @@
+/**
+ * CKEditor plugin: Chrome helper
+ * - Fixes the triple click bug in chrome
+ */
+(function() {
+  "use strict";
+
+  var PLUGIN_NAME = "chromehelper";
+
+  /**
+   * Initializes the plugin
+   */
+  CKEDITOR.plugins.add(PLUGIN_NAME, {
+    init: function(editor) {
+      editor.on("contentDom", function(evt) {
+        init(editor);
+      });
+    }
+  });
+
+  function handleMouseup(editor, event) {
+    if (event.data.$.detail >= 3) {
+      var selection = editor.getSelection();
+      console.log(selection);
+      var range = selection.getRanges()[0];
+      var actualStartContainer = range.startContainer;
+
+      var hasFoundBlock = false;
+      while (
+        !hasFoundBlock &&
+        actualStartContainer.getParent() &&
+        getComputedStyle(actualStartContainer.getParent().$, null).display ==
+          "block"
+      ) {
+        hasFoundBlock = true;
+        actualStartContainer = actualStartContainer.getParent();
+      }
+
+      range.selectNodeContents(actualStartContainer);
+      selection.selectRanges([range]);
+    }
+  }
+
+  function init(editor) {
+    var editable = editor.editable();
+    editor
+      .editable()
+      .attachListener(
+        editor.editable(),
+        "click",
+        handleMouseup.bind(handleMouseup, editor)
+      );
+  }
+})();
